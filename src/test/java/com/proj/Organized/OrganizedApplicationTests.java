a233332d2e92348fa0214ccc2aa8d368723f4d98
Kol-H <arherget@uncg.edu>
2024-02-28 14:33:39 -0500

    Added initial files

---
+++ src/test/java/com/proj/Organized/OrganizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.Organized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
